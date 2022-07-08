8da852a60f84e4393b5a73d9a0751550ba4582d3
Albert Jo <n.hyun.jo@gmail.com>
2022-07-08 02:16:42 -0700

    Add google-analytics SEO

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,17 @@
   import "../app.css";
 </script>
 
+<svelte:head>
+  <title>RedditShuffle - Shuffle your saved Reddit posts</title>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-H1KNNBMQEJ"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-H1KNNBMQEJ');
+  </script>
+</svelte:head>
+
 <slot />
